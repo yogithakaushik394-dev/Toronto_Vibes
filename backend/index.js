33e4044a3f6045caf769d22d361f45531b8e4d14
Yogitha <yogithakaushik394@gmail.com>
2026-04-25 01:11:26 -0400

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,9 @@ const axios = require("axios");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.post("/recommend", async (req, res) => {
